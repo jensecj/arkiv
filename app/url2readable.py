48fce74d85c04975b07b5be52506349212d642cf
Jens Christian Jensen <jensecj@gmail.com>
2019-08-23 14:53:18 +0200

    added a handler for saving an url to a readable .txt file
    using the python port of readability.js

---
+++ app/url2readable.py
@@ -0,0 +1,28 @@
+import requests
+from readability import Document
+from html2text import HTML2Text
+import re
+
+from config import USER_AGENT
+
+
+def generate_readable(url):
+    print("generating readable file...")
+
+    headers = requests.utils.default_headers()
+    headers.update({"User-Agent": USER_AGENT})
+
+    response = requests.get(url, headers=headers)
+    doc = Document(response.text)
+    html = doc.summary()
+
+    parser = HTML2Text()
+    parser.wrap_links = False
+    parser.inline_links = True
+    text = parser.handle(html)
+    text = text.replace("\\n", "")
+    text = re.sub(r"\n\s*\n", "\n\n", text)
+    text = text.strip()
+
+    with open("readable.txt", "w") as f:
+        f.write(text)
