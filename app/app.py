766b6ee86d567cad6798a41a06b17be9c616d780
Jens Christian Jensen <jensecj@gmail.com>
2019-08-25 20:16:15 +0200

    updated app.py, pass config around

--- app/app.py
+++ app/app.py
@@ -16,14 +16,22 @@ from url2warc import generate_warc
 
 
 # TODO: wrap each section in an error handler
-async def process(url):
+async def process(config, url):
     gather_meta(url)
     gather_links(url)
     generate_readable(url)
     await generate_images(url)
     await generate_pdfs(url)
-    generate_archive(url)
-    generate_warc(url)
+
+    links_file = os.path.abspath("links.json")
+    links = []
+
+    if os.path.isfile(links_file):
+        with open(links_file, "r") as f:
+            links = json.load(f)
+
+    generate_warc(config, url)
+    generate_archive(config, url)
 
     print("Archiving complete.")
 
@@ -37,19 +45,26 @@ def read_config():
 
 def main():
     if len(sys.argv) < 2:
-        print("usage: warc-hive <url>")
+        print("usage: arkiver <url>")
         sys.exit()
-
-    logging.basicConfig(format="%(asctime)s %(message)s:", datefmt="%Y-%d-%m %H:%M:%S")
 
     url = sys.argv[1]
     print("archiving " + url)
+
+    config = read_config()
+
+    if config.get("timestamp-output"):
+        logging.basicConfig(
+            format="%(asctime)s %(message)s:", datefmt="%Y-%d-%m %H:%M:%S"
+        )
 
     date = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M")
 
     link = urlparse(url)
 
-    path = date + "--" + link.netloc + link.path + "?" + link.query
+    path = date + "--" + link.netloc + link.path
+    if link.query:
+        path = path + "?" + link.query
 
     path = (
         path.replace(".html", "")
@@ -59,10 +74,8 @@ def main():
         .replace("/", "_")[:75]
     )
 
-    config_path = read_config()
-
-    if config_path:
-        path = os.path.join(config_path["path"], path)
+    if config.get("path"):
+        path = os.path.join(config["path"], path)
     else:
         path = os.path.abspath(path)
 
@@ -71,7 +84,7 @@ def main():
 
     os.chdir(path)
 
-    asyncio.get_event_loop().run_until_complete(process(url))
+    asyncio.get_event_loop().run_until_complete(process(config, url))
 
 
 if __name__ == "__main__":
