57da8498d1a1cf6754c282f4280e814368b8f2b0
Jens Christian Jensen <jensecj@gmail.com>
2019-09-30 18:35:35 +0200

    grab date of first occurance of an url from the webarchive

--- app/url2meta.py
+++ app/url2meta.py
@@ -6,6 +6,27 @@ import logging
 import requests
 import bs4
 
+from shell_utils import shell
+
+
+def get_first_webarchive_record(url):
+    query = "http://web.archive.org/cdx/search/cdx?url="
+    params = "&fl=timestamp&output=json&limit=1"
+    url = f"{query}{url}{params}"
+    cmd = ["curl", "-s", url]
+
+    return_code, stdout, stderr = shell(cmd, log=False)
+
+    if return_code:
+        print(f"failed to get first occurance from webarchive, skipping...")
+        return "unknown"
+
+    record = stdout.replace("\n", " ").strip()
+    j = json.loads(record)
+    timestamp = j[1][0]
+    date = datetime.datetime.strptime(timestamp, "%Y%m%d%H%M%S").isoformat()
+    return date
+
 
 def gather_meta(url):
     print("fetching meta data...")
@@ -13,12 +34,13 @@ def gather_meta(url):
     data = requests.get(url)
     html = bs4.BeautifulSoup(data.text, features="html.parser")
     title = html.title.text.strip()
+    first = get_first_webarchive_record(url)
 
     meta_data = {
         "url": url,
         "title": title,
         "date": datetime.datetime.utcnow().isoformat(),
-        "timestamp": int(time.time()),
+        "first-occurance": first,
     }
 
     with open("meta.json", "w") as f:
