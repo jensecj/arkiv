b3ac9b7b33439e10f567c3f43e27508bd5a8384f
Jens C. Jensen <jensecj@posteo.net>
2020-11-03 16:04:36 +0100

    minor update to url2meta

--- app/url2meta.py
+++ app/url2meta.py
@@ -9,12 +9,12 @@ import bs4
 from shell_utils import shell
 
 
-def get_first_webarchive_record(url):
+def _get_first_webarchive_record(url):
     query = "http://web.archive.org/cdx/search/cdx?url="
     params = "&fl=timestamp&output=json&limit=1"
     url = f"{query}{url}{params}"
-    cmd = ["curl", "-s", url]
 
+    cmd = ["curl", "-s", url]
     return_code, stdout, stderr = shell(cmd, log=False)
 
     if return_code:
@@ -23,8 +23,8 @@ def get_first_webarchive_record(url):
 
     record = stdout.replace("\n", " ").strip()
     j = json.loads(record)
-    timestamp = j[1][0]
-    date = datetime.datetime.strptime(timestamp, "%Y%m%d%H%M%S").isoformat()
+    timestamp = j[1][0] if j else False
+    date = datetime.datetime.strptime(timestamp, "%Y%m%d%H%M%S").isoformat() if timestamp else ""
     return date
 
 
@@ -34,7 +34,7 @@ def gather_meta(url):
     data = requests.get(url)
     html = bs4.BeautifulSoup(data.text, features="html.parser")
     title = html.title.text.strip()
-    first = get_first_webarchive_record(url)
+    first = _get_first_webarchive_record(url)
 
     meta_data = {
         "url": url,
