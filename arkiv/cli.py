a7f3a9cb59da466620e94a9d58a09012a1a3e794
Jens C. Jensen <jens@subst.net>
2021-03-02 17:24:09 +0100

    cli: add -q/--quiet, -d/--dry-run, --json options --json forces --quiet and --dry-run, only outputting extraction info

--- arkiv/cli.py
+++ arkiv/cli.py
@@ -21,10 +21,17 @@ def print_version(ctx, param, value):
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.argument("url", type=str)
+@click.option("--json", is_flag=True)
+@click.option("-d", "--dry-run", is_flag=True)
+@click.option("-q", "--quiet", is_flag=True)
 @click.option("-v", "--verbose", count=True, type=click.IntRange(0, 2))
 @click.option("-V", "--version", is_flag=True, callback=print_version, expose_value=False, is_eager=True,)
-def main(url, verbose):
+def main(url, json, dry_run, quiet, verbose):
+    if json or quiet:
+        verbose = -1
+
     verbosity = {
+        -1: {"root": {"handlers": ["default"], "level": "ERROR"}},
         0: {"root": {"handlers": ["default"], "level": "INFO"}},
         1: {"root": {"handlers": ["extended"], "level": "INFO"}},
         2: {"root": {"handlers": ["extended"], "level": "DEBUG"}},
@@ -34,6 +41,11 @@ def main(url, verbose):
 
     CFG.CONFIG.update(CFG.load())
     CFG.CONFIG.update({"verbosity": verbose})
+
+    CFG.CONFIG.update({"json": json})
+    CFG.CONFIG.update({"dry-run": dry_run})
+
+    log.debug(CFG.CONFIG)
 
     core.archive(url)
 
